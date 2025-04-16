5d6dc5755319ac3721b72c282090772c21fadf2e
bltzxsd <59281378+bltzxsd@users.noreply.github.com>
2025-04-17 00:30:01 +0530

    add more error

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,35 @@
 use crate::shape::Shape;
 
-
+#[derive(Debug, Clone)]
 pub enum TensorError {
-    InconsistentDimensions { 
-        expected: Shape,
-        received: Shape
+    InconsistentDimensions { expected: Shape, received: Shape },
+    MemoryViolation { why: String },
+}
+
+impl std::error::Error for TensorError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            TensorError::InconsistentDimensions {
+                expected: _,
+                received: _,
+            } => None,
+            TensorError::MemoryViolation { why: _ } => None,
+        }
+    }
+}
+
+impl std::fmt::Display for TensorError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TensorError::InconsistentDimensions { expected, received } => {
+                write!(
+                    f,
+                    "inconsistent dimensions. expected: {expected}, received: {received}"
+                )
+            }
+            TensorError::MemoryViolation { why } => {
+                write!(f, "memory handling violation: {why}")
+            }
+        }
     }
 }
