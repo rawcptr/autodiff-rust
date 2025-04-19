4683cc199e3b88a83c39be3a19f3a095cde73443
bltzxsd <59281378+bltzxsd@users.noreply.github.com>
2025-04-19 12:20:28 +0530

    remove dims

--- src/shape.rs
+++ src/shape.rs
@@ -20,24 +20,6 @@ impl From<usize> for Shape {
     }
 }
 
-pub enum Dims {
-    One = 1,
-    Two = 2,
-    Three = 3,
-}
-
-impl From<usize> for Dims {
-    fn from(value: usize) -> Self {
-        use Dims::*;
-        match value {
-            1 => One,
-            2 => Two,
-            3 => Three,
-            x => panic!("got {x} dims but only three dims are supported"),
-        }
-    }
-}
-
 impl Shape {
     pub fn change(&mut self, shape: impl Into<Shape>) {
         let shape = shape.into();
@@ -46,8 +28,8 @@ impl Shape {
     }
 
     #[inline]
-    pub fn ndims(&self) -> Dims {
-        self.0.len().into()
+    pub fn ndims(&self) -> usize {
+        self.0.len()
     }
 
     #[inline]
@@ -71,17 +53,13 @@ impl Index<usize> for Shape {
     type Output = usize;
 
     fn index(&self, index: usize) -> &Self::Output {
-        assert!(index < self.ndims() as usize);
+        assert!(index < self.0.len());
         &self.0[index]
     }
 }
 
 impl Display for Shape {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self.ndims() {
-            Dims::One => write!(f, "Shape({})", self[0]),
-            Dims::Two => write!(f, "Shape({}, {})", self[0], self[1]),
-            Dims::Three => write!(f, "Shape({}, {}, {})", self[0], self[1], self[2]),
-        }
+        write!(f, "Shape({:?})", &self.0)
     }
 }
